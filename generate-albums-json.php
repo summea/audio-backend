b3e2d20c6de1b8c0624965818f612e28d15ce9c0
summea <summea@gmail.com>
2021-09-04 17:34:41 -0700

    Early commit

---
+++ generate-albums-json.php
@@ -0,0 +1,34 @@
+<?php
+
+$db = new PDO('sqlite:songs.db');
+$albumRows = $db->query('SELECT * FROM albums')->fetchAll();
+$output['albums'] = [];
+foreach ($albumRows as $albumRow) {
+    $albumData = [];
+    $albumData['id'] = $albumRow['id'];
+    $albumData['coverImage'] = $albumRow['coverImageUrl'];
+    $albumData['name'] = $albumRow['name'];
+    // ref: https://www.php.net/manual/en/pdo.prepare.php
+    $sth = $db->prepare('
+        SELECT * FROM albums_songs
+        LEFT JOIN songs
+        ON albums_songs.songId = songs.id
+        WHERE albumId = :albumId
+        ORDER BY albumId, albumSongNumber
+    ');
+    $sth->execute([':albumId' => $albumRow['id']]);
+    $songRows = $sth->fetchAll();
+    $albumData['songs'] = [];
+    foreach ($songRows as $songRow) {
+        array_push($albumData['songs'], [
+            'id' => $songRow['id'],
+            'name' => $songRow['name'],
+            'url' => $songRow['songUrl'],
+            'albumSongNumber' => $songRow['albumSongNumber']
+        ]);
+    }
+    array_push($output['albums'], $albumData);
+}
+
+// ref: https://www.php.net/manual/en/function.file-put-contents.php
+file_put_contents('albums.json', json_encode($output));
